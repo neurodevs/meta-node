fd135f0d1f42b1fe81c526311b39af69c0c51fc0
ericthecurious <hello@ericthecurious.com>
2025-10-18 23:30:14 -0700

    patch: create demo script for VscodeSnippetKeybinder

---
+++ src/scripts/runSnippetKeybinder.ts
@@ -0,0 +1,19 @@
+import VscodeSnippetKeybinder from '../modules/VscodeSnippetKeybinder'
+
+async function main() {
+    console.log('Running VscodeSnippetKeybinder...')
+
+    const instance = VscodeSnippetKeybinder.Create({
+        name: 'Test the thingy',
+        description: 'Test the thingy! Do it!',
+        lines: ['@test()', 'protected static async exampleTest() {}'],
+        keybinding: 'f5',
+    })
+
+    await instance.run()
+}
+
+main().catch((err) => {
+    console.error(err)
+    process.exit(1)
+})
