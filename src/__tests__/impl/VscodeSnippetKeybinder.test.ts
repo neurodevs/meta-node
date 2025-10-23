b7cb98c2fae765f2e42573666498b1e8643e5d89
ericthecurious <hello@ericthecurious.com>
2025-10-22 19:54:04 -0700

    patch: fix extra char typo in test

--- src/__tests__/impl/VscodeSnippetKeybinder.test.ts
+++ src/__tests__/impl/VscodeSnippetKeybinder.test.ts
@@ -123,7 +123,7 @@ export default class VscodeSnippetKeybinderTest extends AbstractPackageTest {
         return {
             ...this.originalSnippet,
             [this.fakeName]: {
-                scope: 'stypescript,typescriptreact',
+                scope: 'typescript,typescriptreact',
                 prefix: this.fakePrefix,
                 body: this.fakeLines,
                 description: this.fakeDescription,
